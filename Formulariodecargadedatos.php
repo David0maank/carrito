7cb05e5c9a9997f43d55575749a3553cb8d70adf
David0maank <74891757+David0maank@users.noreply.github.com>
2022-07-22 17:57:59 -0500

    Add files via upload

---
+++ Formulariodecargadedatos.php
@@ -0,0 +1,48 @@
+<html>
+
+<head>
+
+<title>insertar</title>
+
+</head>
+
+<body>
+<left>
+<h1>usuarios</h1>
+
+<form action="http://localhost/carrito/pagina22.php" method="post">
+
+
+nombre de usuario:
+<input type="text" name="name"<br>
+
+<br>
+Ingrese su email
+
+<input type="text" name="email">
+<br>
+Ingrese su direccion
+
+<input type="text" name="address">
+<br>
+
+ingrese su contraseña
+
+<input type="text" name="contraseña">
+
+<br>
+
+ingrese su telefono
+<input type="text" name="phone">
+
+<br>
+
+<input type="submit" value="Registrar">
+
+
+</center>
+</form>
+
+</body>
+
+</html>
---
+++ log.php
@@ -0,0 +1,46 @@
+
+<?php
+
+
+$dbhost="localhost";
+    $dbuser="root";
+    $dbpass="";
+    $dbname="programacionnet";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+if (!$conn)
+{
+die("No hay conexion:g".mysqli_connect_error());
+}
+
+$nombre=$_POST["id"];
+$pass = $_POST["contraseña"];
+
+$query = mysqli_query($conn,"SELECT * FROM customers WHERE name ='".$nombre."'and contraseña = '".$pass."'");
+$nr =mysqli_num_rows($query);
+
+if($nr==1)
+{
+header("Location:http://localhost/carrito/index.php");
+
+echo"BIenvenido:" .$nombre;
+
+
+
+
+
+}
+else if ($nr == 0)
+{
+
+
+
+header("location:inicio.html");
+echo"No ingreso";
+}
+
+
+?>
+
+
+?>
